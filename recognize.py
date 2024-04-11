65d950c6167db8ca6f3091ee65ecc181cda87ebf
acwDevs <acw36541@gmail.com>
2024-04-10 20:27:48 -0700

    Adding token

--- recognize.py
+++ recognize.py
@@ -30,54 +30,54 @@ else:
 savePath = path.dirname(path.realpath(__file__))
 
 # #link of the video to be downloaded 
-# if len(sys.argv) < 2:
-#     print("Please provide a link to the video.")
-#     sys.exit(1)
+if len(sys.argv) < 2:
+    print("Please provide a link to the video.")
+    sys.exit(1)
 
-# print("Downloading video from", sys.argv[1])
-# link = sys.argv[1]
+print("Downloading video from", sys.argv[1])
+link = sys.argv[1]
 
-# try: 
-#     # object creation using YouTube 
-#     yt = YouTube(link) 
-# except: 
-#     #to handle exception 
-#     print("Connection Error.") 
+try: 
+    # object creation using YouTube 
+    yt = YouTube(link) 
+except: 
+    #to handle exception 
+    print("Connection Error.") 
 
-# # Get all streams and filter for audio only
-# webm_streams = yt.streams.filter(only_audio=True)
+# Get all streams and filter for audio only
+webm_streams = yt.streams.filter(only_audio=True)
 
-# # get the video with the highest resolution
-# d_audio = webm_streams[-1]
+# get the video with the highest resolution
+d_audio = webm_streams[-1]
 
-# try: 
-#     # downloading the video 
-#     d_audio.download(savePath)
-#     print('Video downloaded successfully!')
-# except: 
-#     print("Some Error.")
+try: 
+    # downloading the video 
+    d_audio.download(savePath)
+    print('Video downloaded successfully!')
+except: 
+    print("Some Error.")
 
-# webmPath = path.join(savePath, "audio.webm")
-# # Rename the file to audio.wav
-# for file in listdir(savePath):
-#     if file.endswith('.webm'):
-#         rename(path.join(savePath, file), webmPath)
-#         break
+webmPath = path.join(savePath, "audio.webm")
+# Rename the file to audio.wav
+for file in listdir(savePath):
+    if file.endswith('.webm'):
+        rename(path.join(savePath, file), webmPath)
+        break
 
 # # Path to the audio file
 audioPath = path.join(savePath, "audio.wav")
 
 
 # Convert the audio file to wav format
-# if path.isfile(audioPath):
-#     remove(audioPath)
-# clip = mp.AudioFileClip(webmPath)
-# clip.write_audiofile("audio.wav")
-# remove("audio.webm")
+if path.isfile(audioPath):
+    remove(audioPath)
+clip = mp.AudioFileClip(webmPath)
+clip.write_audiofile("audio.wav")
+remove("audio.webm")
 
-# # Reduce noise in the audio
-rate, data = wavfile.read(audioPath)
-data = nr.reduce_noise(data, rate)
+# # # Reduce noise in the audio
+# rate, data = wavfile.read(audioPath)
+# data = nr.reduce_noise(data, rate)
 
 # with sr.Microphone() as source:
 #     r = sr.Recognizer()
@@ -99,13 +99,13 @@ for turn, _, speaker in diarization.itertracks(yield_label=True):
     print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
 
 # Group consecutive segments with the same speaker
-i = 0
-while i < len(segments) - 1:
-    if segments[i][2] == segments[i + 1][2]:
-        segments[i] = (segments[i][0], segments[i + 1][1], segments[i][2])
-        segments.pop(i + 1)
-    else:
-        i += 1
+# i = 0
+# while i < len(segments) - 1:
+#     if segments[i][2] == segments[i + 1][2]:
+#         segments[i] = (segments[i][0], segments[i + 1][1], segments[i][2])
+#         segments.pop(i + 1)
+#     else:
+#         i += 1
 
 # # Remove and clips that are too short
 # i = 0
@@ -129,7 +129,7 @@ with sr.AudioFile(audioPath) as source:
     r.dynamic_energy_threshold = True
     audio = r.record(source) 
     for i in range(len(segments)):
-        start = (segments[i][0] * 1000)
+        start = abs((segments[i][0] * 1000) - 300)
         end = (segments[i][1] * 1000)
         speaker = segments[i][2]
         clip = audio.get_segment(start, end)
